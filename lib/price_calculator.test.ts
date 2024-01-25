c78edcbdac4e8435d5b6de54aebe9200caee99eb
Phornphatch <phornphatch@gmail.com>
2024-01-25 18:13:37 +0200

    feat: add distance fee function and tests

--- lib/price_calculator.test.ts
+++ lib/price_calculator.test.ts
@@ -1,5 +1,5 @@
 import { expect, test, describe } from "vitest";
-import { calculateSurcharge } from "./price_calculator";
+import { calculateSurcharge, calculateDistanceFee } from "./price_calculator";
 
 describe("calculateSurcharge", () => {
   test("If the cart value is less than 10€, a small order surcharge is added to the delivery price", () => {
@@ -12,5 +12,27 @@ describe("calculateSurcharge", () => {
 
   test("If the cart value is more than 10€, a small order surcharge is not added to the delivery price", () => {
     expect(calculateSurcharge(12)).toBe(0);
+  });
+});
+
+describe("calculateDistanceFee", () => {
+  test("delivery fee for the first 1000 meters (=1km) is 2€", () => {
+    expect(calculateDistanceFee(999)).toBe(2);
+  });
+
+  test("distance 1000, 2", () => {
+    expect(calculateDistanceFee(1000)).toBe(2);
+  });
+
+  test("distance 1001 to 1499, 3", () => {
+    expect(calculateDistanceFee(1499)).toBe(3);
+  });
+
+  test("1500, 3", () => {
+    expect(calculateDistanceFee(1500)).toBe(3);
+  });
+
+  test("1501, 4", () => {
+    expect(calculateDistanceFee(1501)).toBe(4);
   });
 });
--- lib/price_calculator.ts
+++ lib/price_calculator.ts
@@ -7,6 +7,15 @@ export function calculateSurcharge(
   return 0
 }
 
+export function calculateDistanceFee(
+  distanceInMeters: number,
+): number {
+  if (distanceInMeters <= 1000) {
+    return 2;
+  }
+  return Math.ceil(2 + ((distanceInMeters - 1000) / 500));
+}
+
 export function calculate(
   cartValueInEuros: number,
   distanceInMeters: number,
